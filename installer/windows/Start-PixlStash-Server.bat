2d2054cf708b5bcb389f6c8d66ff9be09fb55943
Gaute Lindkvist <lindkvis@gmail.com>
2026-03-17 08:22:34 +0100

    Fall back to use python -m pixlstash.app if the executable wasn't created on Windows

--- installer/windows/Start-PixlStash-Server.bat
+++ installer/windows/Start-PixlStash-Server.bat
@@ -2,7 +2,13 @@
 set PIXLSTASH_APP_DIR=%~dp0
 set PIXLSTASH_PORT=9537
 
-start "" "%PIXLSTASH_APP_DIR%venv\Scripts\pixlstash-server.exe" %*
+if exist "%PIXLSTASH_APP_DIR%venv\Scripts\pixlstash-server.exe" (
+    start "" "%PIXLSTASH_APP_DIR%venv\Scripts\pixlstash-server.exe" %*
+) else (
+    start "" "%PIXLSTASH_APP_DIR%venv\Scripts\python.exe" -m pixlstash.app %*
+)
 
+powershell -NoProfile -Command ^
+  "$url = 'http://localhost:%PIXLSTASH_PORT%/version'; $deadline = [DateTime]::UtcNow.AddSeconds(60); while ([DateTime]::UtcNow -lt $deadline) { try { Invoke-WebRequest -Uri $url -UseBasicParsing -TimeoutSec 2 -ErrorAction Stop | Out-Null; break } catch { Start-Sleep -Seconds 1 } }; Start-Process $url"
 powershell -NoProfile -Command ^
   "$url = 'http://localhost:%PIXLSTASH_PORT%/version'; $deadline = [DateTime]::UtcNow.AddSeconds(60); while ([DateTime]::UtcNow -lt $deadline) { try { Invoke-WebRequest -Uri $url -UseBasicParsing -TimeoutSec 2 -ErrorAction Stop | Out-Null; break } catch { Start-Sleep -Seconds 1 } }; Start-Process $url"
