1202d5db220a855b89df8bd23b4cfccfddc71198
Gaute Lindkvist <lindkvis@gmail.com>
2026-05-15 16:35:33 +0200

    Update Whats new links

--- website/nav.js
+++ website/nav.js
@@ -5,7 +5,7 @@
     { href: "features.html", label: "Features" },
     {
       href: "whatsnew.html",
-      label: "What's New in 1.1",
+      label: "What's New in 1.2",
       className: "nav-link--whatsnew",
     },
     { href: "api.html", label: "API" },
